732eb0c0d9ac60fbeda76341a9f9ea7b55ed4bca
d4v1-sudo <139714828+d4v1-sudo@users.noreply.github.com>
2024-08-24 13:10:13 -0300

    Update img2img_color.py

--- img2img_color.py
+++ img2img_color.py
@@ -1,13 +1,8 @@
-"""
-@author: Viet Nguyen <nhviet1009@gmail.com>
-"""
+import time
 import argparse
-
-import cv2
-import numpy as np
 from PIL import Image, ImageDraw, ImageOps
+import numpy as np
 from utils import get_data
-
 
 def get_args():
     parser = argparse.ArgumentParser("Image to ASCII")
@@ -22,8 +17,9 @@ def get_args():
     args = parser.parse_args()
     return args
 
-
 def main(opt):
+    start_time = time.time()
+    
     if opt.background == "white":
         bg_code = (255, 255, 255)
     else:
@@ -31,39 +27,58 @@ def main(opt):
     char_list, font, sample_character, scale = get_data(opt.language, opt.mode)
     num_chars = len(char_list)
     num_cols = opt.num_cols
-    image = cv2.imread(opt.input, cv2.IMREAD_COLOR)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    height, width, _ = image.shape
+    
+    image = Image.open(opt.input).convert("RGB")
+    width, height = image.size
+
+    char_bbox = font.getbbox(sample_character)
+    char_width = char_bbox[2] - char_bbox[0]
+    char_height = char_bbox[3] - char_bbox[1]
+
     cell_width = width / opt.num_cols
-    cell_height = scale * cell_width
+    cell_height = (char_height / char_width) * cell_width
     num_rows = int(height / cell_height)
+    
     if num_cols > width or num_rows > height:
         print("Too many columns or rows. Use default setting")
         cell_width = 6
         cell_height = 12
         num_cols = int(width / cell_width)
         num_rows = int(height / cell_height)
-    char_width, char_height = font.getsize(sample_character)
+
     out_width = char_width * num_cols
     out_height = scale * char_height * num_rows
     out_image = Image.new("RGB", (out_width, out_height), bg_code)
     draw = ImageDraw.Draw(out_image)
+    
+    np_image = np.array(image)
+    
     for i in range(num_rows):
         for j in range(num_cols):
-            partial_image = image[int(i * cell_height):min(int((i + 1) * cell_height), height),
-                            int(j * cell_width):min(int((j + 1) * cell_width), width), :]
-            partial_avg_color = np.sum(np.sum(partial_image, axis=0), axis=0) / (cell_height * cell_width)
-            partial_avg_color = tuple(partial_avg_color.astype(np.int32).tolist())
-            char = char_list[min(int(np.mean(partial_image) * num_chars / 255), num_chars - 1)]
+            left = int(j * cell_width)
+            top = int(i * cell_height)
+            right = min(int((j + 1) * cell_width), width)
+            bottom = min(int((i + 1) * cell_height), height)
+            partial_image = np_image[top:bottom, left:right, :]
+            
+            partial_avg_color = np.mean(partial_image, axis=(0, 1)).astype(np.int32)
+            partial_avg_color = tuple(partial_avg_color.tolist())
+            char_index = int(np.mean(partial_image) * num_chars / 255)
+            char = char_list[min(char_index, num_chars - 1)]
+            
             draw.text((j * char_width, i * char_height), char, fill=partial_avg_color, font=font)
-
+    
     if opt.background == "white":
         cropped_image = ImageOps.invert(out_image).getbbox()
     else:
         cropped_image = out_image.getbbox()
     out_image = out_image.crop(cropped_image)
+    
     out_image.save(opt.output)
 
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("Execution time: ", elapsed_time, "seconds")
 
 if __name__ == '__main__':
     opt = get_args()
